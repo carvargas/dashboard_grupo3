faf2ee010026336369c4b760b4dc7089adc12f5e
Giuliano <gdepalma@santandertecnologia.com.ar>
2022-05-28 14:17:48 -0300

    produtList mobile

--- dashboard/src/pages/Products/ProductList/components/ProductListHeader.js
+++ dashboard/src/pages/Products/ProductList/components/ProductListHeader.js
@@ -7,8 +7,13 @@ const ProductListHeader = () => {
         <ul>
             <li><p>Productos</p></li>
             <div>
-                <li><button>(?)</button></li>
-                <li><button>(+)</button></li>
+                <li className='searchContainer'>
+                  <label htmlFor='search'><i className="fa-solid fa-magnifying-glass searchIcon"></i></label>
+                  <input type='text' id='search' name='search'/>
+                </li>
+                <li className='addContainer'>
+                  <button><i className="fa-solid fa-plus"></i></button>
+                </li>
             </div>
             
         </ul>
