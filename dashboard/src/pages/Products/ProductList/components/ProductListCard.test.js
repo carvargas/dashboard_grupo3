1b2d7627821927b0f492c86ebc4efbc4d5c02d34
axelfischbein <axelfischbein@gmail.com>
2022-06-06 16:20:35 -0300

    productlist test.js terminado

--- dashboard/src/pages/Products/ProductList/components/ProductListCard.test.js
+++ dashboard/src/pages/Products/ProductList/components/ProductListCard.test.js
@@ -1,19 +1,21 @@
-import {screen, render} from '@testing-library/react';
-import {MemoryRouter} from 'react-router-dom';
+//Testing
+import { render,screen } from "@testing-library/react";
+//dependencias emocionales
+import { BrowserRouter } from "react-router-dom";
 //componentes
-import ProductListCard from './ProductListCard';
+import ProductListCard from "./ProductListCard";
 
-test('se renderiza el componente productListCard', () => { 
-    
-    const product = {
-        _id:"2",
+//Objeto literal
+let product = 
+    {
+        _id:"3",
         category:"Alimentos",
-        nombre:"Alfajores 70% Cacao Puro x 9 uds",
-        description:"Alfajores rellenos con dulce de leche con cobertura de chocolate 70% Cacao Puro x 9 unidades",
-        src:"https://tienda.havanna.com.ar/wp-content/uploads/2020/10/alfajor70caco.jpg",
-        alt:"foto Alfajores Cacao",
-        puntos:15250,
-        gallery:["https://tienda.havanna.com.ar/wp-content/uploads/2020/10/alfajor70caco.jpg"],
+        nombre:"ALFAJORES CHOCOLATE x 12 uds",
+        description:"Alfajores rellenos con dulce de leche con cobertura de chocolate x 12 unidades.",
+        src:"https://tienda.havanna.com.ar/wp-content/uploads/2020/04/alfajorChocolatex12.jpg",
+        alt:"foto Alfajores Chocolate",
+        puntos:16000,
+        gallery:["https://tienda.havanna.com.ar/wp-content/uploads/2020/04/alfajorChocolatex12.jpg"],
         mostwanted:false,
         suggested:false,
         stock:0,
@@ -21,20 +23,46 @@ test('se renderiza el componente productListCard', () => {
         __v:0
     }
 
-    render(<ProductListCard product={product}/>, {wrapper:MemoryRouter});
-    screen.debug();
 
-});
+//Funcion test
+describe('testiando-ProductListCard', () => { 
+    
+    beforeEach( () => {
+        render(
+            <BrowserRouter>
+                <ProductListCard product = {product}/>
+            </BrowserRouter>
+        )
+    })
+    
+    test('Renderizando li', () => { 
+        let li = screen.getByRole("listitem");
+        expect(li).toBeInTheDocument();
+     })
+
+     test('imagen esta?', () => { 
+         let imagen = screen.getByRole("img");
+         expect(imagen).toBeInTheDocument();
+         expect(imagen).toHaveAttribute("src",product.src);
+         expect(imagen).toHaveAttribute("alt",product.alt);
+     })
+
+     test('titulo', () => { 
+        let titulo = screen.getByRole("heading");
+        expect(titulo).toBeInTheDocument();
+        expect(titulo).toHaveTextContent(product.nombre);
+    })
+
+    test('paragraph ID', () => { 
+        let paragraph = screen.getByText(`#${product._id}`);
+        expect(paragraph).toBeInTheDocument();
+        expect(paragraph).toHaveTextContent(product._id);
+    })
+
+    test('boton funciona?', () => { 
+        let but = screen.getByRole("button");
+        expect(but).toBeInTheDocument();
+    })
 
 
-//Componentes interactivos: 
-// formularios, botones, inputs, etc.
-
-//Componentes sencillos: 
-// div, span, img, etc.
-
-//hooks personalizados:
-// useState, useEffect, useContext, useRedirect, etc.
-
-//Servicios y utilidades:
-// axios, moment, lodash, etc.
+ })
